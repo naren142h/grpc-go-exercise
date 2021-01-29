5d9b79db4967dab3fedde978da9e24ce3b5dae2d
Naren Hari Krishnan <naren.hari.krishnan@ericsson.com>
2021-01-29 14:21:00 +0100

    finished implementing funcs

--- server/main.go
+++ server/main.go
@@ -1,5 +1,20 @@
-package server
+package main
+
+import (
+    "google.golang.org/grpc"
+    "os"
+
+    pb "grpc-tutorial/proto"
+)
 
 func main() {
+    port := os.Getenv("PORT")
+    if port == "" {
+        port = "8080"
+    }
 
+    srv := grpc.NewServer()
+    pb.RegisterCalculatorServiceServer(srv, NewServer())
+
+    listen, err :=
 }
--- server/server.go
+++ server/server.go
@@ -3,6 +3,8 @@ package server
 import (
 	"context"
 	pb "grpc-tutorial/proto"
+	"log"
+	"fmt"
 )
 
 var _ pb.CalculatorServiceServer = (*Server)(nil)
@@ -15,9 +17,15 @@ func NewServer() *Server {
 }
 
 func (s *Server) Add(ctx context.Context, req *pb.Request) (*pb.Response, error) {
-
+    a := req.GetA()
+    b := req.GetB()
+    res := a + b
+    return &pb.Response{result: res}, nil
 }
 
 func (s *Server) Multiply(ctx context.Context, req *pb.Request) (*pb.Response, error) {
-
+    a := req.GetA()
+    b := req.GetB()
+    res := a * b
+    return &pb.Response{result: res}, nil
 }
